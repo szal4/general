6981ae1eb4faae5c79bbb2d0d6742765bc9e4f27
Sajal Jain <sajaljain4444g@gmail.com>
2020-08-12 12:15:30 +0530

    Update sift_arr_left.cpp

--- sift_arr_left.cpp
+++ sift_arr_left.cpp
@@ -1,3 +1,4 @@
+//Given an array a of n integers and a number,d , perform d left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
 int gcdd(int a,int b){
     if(b==0)
     return a;
